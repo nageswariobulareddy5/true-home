034ffcefd8db8b1e9b76a646009ac6aed9ba296c
Nageswari41 <nageswariobulareddy5@gmail.com>
2026-06-11 17:44:22 +0530

    Initial Commit

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -564,52 +564,39 @@ function Contact() {
       window.location.hash = "#portfolio";
     }, 100);
   };
+const handleSubmit = async (e: any) => {
+  e.preventDefault();
 
-  const handleSubmit = async (e: any) => {
-    e.preventDefault();
+  const form = e.target;
 
-    const form = e.target;
+  try {
+    const formData = new FormData();
 
-    const data = {
-  name: form.name.value,
-  email: form.email.value,
-  preferredLocation: form.location.value,
-  mobileNumber: form.mobile.value,
-  propertyType: form.property.value,
-  budgetRange: form.budget.value,
-  propertyRequirement: form.message.value,
+    formData.append("entry.249874652", form.name.value);
+    formData.append("entry.1097084303", form.email.value);
+    formData.append("entry.1237868596", form.location.value);
+    formData.append("entry.77652021", form.mobile.value);
+    formData.append("entry.257641967", form.property.value);
+    formData.append("entry.664015142", form.budget.value);
+    formData.append("entry.1375262967", form.message.value);
+
+    await fetch(
+      "https://docs.google.com/forms/d/e/1FAIpQLSeuqozfd2Msc-8KREXgzhwoQ3T5ZcXoL_1X8YyL_Hi5TeftdQ/formResponse",
+      {
+        method: "POST",
+        mode: "no-cors",
+        body: formData,
+      }
+    );
+
+    form.reset();
+    setSubmitted(true);
+  } catch (error) {
+    console.error("Submission Error:", error);
+    alert("Failed to submit form");
+  }
 };
 
-    console.log(data);
-
-    try {
-      await fetch(
-        "https://script.google.com/macros/s/AKfycbzdjXi-npphM2Ld1XLsKKT3hFpCz7GBABABdQDl_bRg2vBAeN5H5naoi-vEYy5WqLU1Gw/exec",
-        {
-          method: "POST",
-          mode: "no-cors",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(data),
-        }
-      );
-
-      form.reset();
-      setSubmitted(true);
-
-      setTimeout(() => {
-        window.scrollTo({
-          top: 0,
-          behavior: "smooth",
-        });
-      }, 100);
-
-    } catch (error) {
-      console.error(error);
-      alert("Failed to submit form");
-    }
-  };
 
   if (submitted) {
     return (
@@ -746,6 +733,8 @@ function Contact() {
     </section>
   );
 }
+
+
 function Footer() {
   return (
     <footer className="bg-black text-white py-16">
